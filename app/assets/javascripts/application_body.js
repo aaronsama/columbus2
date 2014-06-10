56631391b83944375ce26491a1b1a447791e4311
Adolfo Villafiorita <adolfo.villafiorita@me.com>
2014-06-10 22:46:11 +0200

    fixes to layout (row width is now 100%) datatables

--- app/assets/javascripts/application_body.js
+++ app/assets/javascripts/application_body.js
@@ -1,5 +1,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require dataTables/jquery.dataTables
+//= require dataTables/jquery.dataTables.foundation
 
 $(function(){ $(document).foundation(); });
+
+$('.datatable').dataTable({
+    "sPaginationType": "foundation",
+    "paging": false
+});
