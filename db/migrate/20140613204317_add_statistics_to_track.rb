d4019e68f8855561d07beeec60e2d49888001553
Adolfo Villafiorita <adolfo.villafiorita@me.com>
2014-06-13 23:32:57 +0200

    added updating of data in migration

--- db/migrate/20140613204317_add_statistics_to_track.rb
+++ db/migrate/20140613204317_add_statistics_to_track.rb
@@ -1,5 +1,5 @@
 class AddStatisticsToTrack < ActiveRecord::Migration
-  def change
+  def up
     add_column :tracks, :start_date, :datetime
     add_column :tracks, :duration, :integer
 
@@ -10,5 +10,23 @@ class AddStatisticsToTrack < ActiveRecord::Migration
     
     add_column :tracks, :min_height, :integer
     add_column :tracks, :max_height, :integer
+
+    Track.all.each do |track|
+      track.cache_statistics V900Track.new(track.track.path)
+    end
   end
+
+  def down
+    remove_column :tracks, :start_date
+    remove_column :tracks, :duration
+
+    remove_column :tracks, :distance
+
+    remove_column :tracks, :min_speed
+    remove_column :tracks, :max_speed
+    
+    remove_column :tracks, :min_height
+    remove_column :tracks, :max_height
+  end
+
 end
